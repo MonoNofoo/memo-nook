33f047897ca55d60dfbc78f3f9c576cada68ea8e
mono_nofoo <dhntyt@gmail.com>
2024-06-30 23:31:08 +0900

    generateMetadata非同期だとｓãstartのときだけエラーになるのでいったん削除

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -1,22 +1,9 @@
-import { Metadata } from 'next';
+import { FC } from 'react';
 import * as styles from '@src/app/article/[slug]/page.css';
 import { Article } from '@src/model/article/Article';
 import { ArticleSlug } from '@src/model/article/ArticleSlug';
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<Metadata> {
-  const { metadata } = await import(`@src/app/article/_mdx/${params.slug}.mdx`);
-
-  return {
-    title: metadata.title,
-    description: metadata.description,
-  };
-}
-
-const Page = async ({ params }: { params: { slug: string } }) => {
+const Page: FC<{ params: { slug: string } }> = async ({ params }) => {
   const { default: Content, metadata } = await import(
     `@src/app/article/_mdx/${params.slug}.mdx`
   );
